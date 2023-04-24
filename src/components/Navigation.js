d1be4cb684bc03e14efaa2db9a404105494cfff7
keyxcode <keyxcode@gmail.com>
2023-04-25 00:49:59 +0700

    Style Navigation component

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,12 +1,17 @@
 import { Link } from "react-router-dom";
+import { Header, Group, rem } from "@mantine/core";
+
+const HEADER_HEIGHT = rem(60);
 
 const Navigation = ({ user, handleLogout }) => (
-  <div>
-    <Link to={"/"}>blogs</Link>
-    <Link to={"/users"}>users</Link>
-    {user && `${user.name} logged in`}
-    {user && <button onClick={handleLogout}>logout</button>}
-  </div>
+  <Header height={HEADER_HEIGHT}>
+    <Group spacing={20}>
+      <Link to={"/"}>blogs</Link>
+      <Link to={"/users"}>users</Link>
+      {user && `${user.name} logged in`}
+      {user && <button onClick={handleLogout}>logout</button>}
+    </Group>
+  </Header>
 );
 
 export default Navigation;
