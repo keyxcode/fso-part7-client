ac8f0214c0d0888e04d55f4db90e222215212c75
keyxcode <keyxcode@gmail.com>
2023-04-25 01:39:06 +0700

    Style user's blog list

--- src/routes/User.js
+++ src/routes/User.js
@@ -1,16 +1,17 @@
-const User = ({ user }) => {
-  if (!user) return null;
-  return (
-    <div>
-      <h1>{user.name}</h1>
-      <h2>added blogs</h2>
-      <ul>
-        {user.blogs.map((b) => (
-          <li key={b.id}>{b.title}</li>
-        ))}
-      </ul>
-    </div>
-  );
-};
+import { TextInput, Button, Paper, Anchor } from "@mantine/core";
+
+const User = ({ user }) => (
+  <div>
+    <h1>{user.name}</h1>
+    <h2>added blogs</h2>
+    <ul>
+      {user.blogs.map((b) => (
+        <Paper shadow="xs" p="md" withBorder key={b.id}>
+          {b.title}
+        </Paper>
+      ))}
+    </ul>
+  </div>
+);
 
 export default User;
