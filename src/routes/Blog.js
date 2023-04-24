b6fe107a26bc45b26969aa6fe5e5baa700c41249
keyxcode <keyxcode@gmail.com>
2023-04-24 13:50:38 +0700

    Implement a working Blog route

--- src/routes/Blog.js
+++ src/routes/Blog.js
@@ -1,3 +1,14 @@
-const Blog = () => {};
+const Blog = ({ blog }) => (
+  <div>
+    <h1>{blog.title}</h1>
+    <div>
+      <a href={blog.url}>{blog.url}</a>
+    </div>
+    <div>
+      {blog.likes} likes <button>like</button>
+    </div>
+    <div>added by {blog.author}</div>
+  </div>
+);
 
 export default Blog;
